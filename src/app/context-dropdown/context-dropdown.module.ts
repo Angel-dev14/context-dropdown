d90bf90d655c551053863ad04729f33b3770a226
angel.petrushevski <angel.petrushevski@sorsix.com>
2024-02-20 20:28:48 +0100

    initial

---
+++ src/app/context-dropdown/context-dropdown.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContextDropdownView } from './view/context-dropdown.view';
+import { ContextDropdownDirective } from './directives/context-dropdown.directive';
+
+const views = [
+  ContextDropdownView,
+];
+
+const directives = [
+  ContextDropdownDirective,
+]
+
+@NgModule({
+  declarations: [
+    ...views,
+    ...directives
+  ],
+  exports: [
+    ...views,
+    ...directives
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ContextDropdownModule { }
